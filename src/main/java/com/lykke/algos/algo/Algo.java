69848474c70404e0840c848ebc6175eb9c7ba3a9
Nick Milovanov <nmil@itransformers.net>
2017-11-21 19:57:59 +0200

    Make main class parametrized based on the package!

--- src/main/java/com/lykke/algos/algo/Algo.java
+++ src/main/java/com/lykke/algos/algo/Algo.java
@@ -3,12 +3,12 @@ package com.lykke.algos.algo;
 import com.lykke.hft.ApiClient;
 import com.lykke.hft.client.AssetPairsApi;
 import com.lykke.hft.client.OrderBooksApi;
+import com.lykke.hft.client.OrdersApi;
 import com.lykke.hft.model.AssetPairModel;
+import com.lykke.hft.model.LimitOrderRequest;
 import com.lykke.hft.model.OrderBook;
-import feign.RequestInterceptor;
 
 import java.util.List;
-import java.util.Map;
 
 public class Algo {
 
@@ -16,6 +16,7 @@ public class Algo {
 
 	OrderBooksApi orderBooksApi;
 	AssetPairsApi assetPairsApi;
+	OrdersApi ordersApi;
 
 	public Algo(ApiClient api, int counterMax) {
 		init(api);
@@ -24,6 +25,7 @@ public class Algo {
 	public void init(ApiClient api){
 		this.orderBooksApi = api.buildClient(OrderBooksApi.class);
 		this.assetPairsApi = api.buildClient(AssetPairsApi.class);
+		this.ordersApi = api.buildClient(OrdersApi.class);
 	}
 
 	public void run() throws InterruptedException {
@@ -43,8 +45,16 @@ public class Algo {
 			if (counter==counterMax){
 				break;
 			}
+			LimitOrderRequest limitOrderRequest = new LimitOrderRequest();
+			limitOrderRequest.assetPairId("AUDCHF");
+			limitOrderRequest.setOrderAction(LimitOrderRequest.OrderActionEnum.BUY);
+			limitOrderRequest.setPrice(2.34);
+			limitOrderRequest.setVolume(1.1);
+			ordersApi.placeLimitOrder("dc305d-8add-4fc2-9efa-387d3bfe4b43",limitOrderRequest);
 		}
 	}
+
+
 
 	private List<OrderBook> getOrderBooks(){
 		return orderBooksApi.orderBooks();
@@ -56,11 +66,11 @@ public class Algo {
 
 	public static void main(String[] args) throws InterruptedException {
 		ApiClient apiClient = new ApiClient();
-		Map<String, RequestInterceptor> authMap = apiClient.getApiAuthorizations();
+
 
 		apiClient.setBasePath("https://hft-service-dev.lykkex.net");
 
-
+//		apiClient.setApiKey("4edc305d-8add-4fc2-9efa-387d3bfe4b43");
 		Algo algo  = new Algo(apiClient, 1000);
 		algo.run();
 	}
